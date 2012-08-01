1ff475434ee86259f622842e93277c2369265391
Rich Buggy <rich@buggy.id.au>
2012-08-01 11:23:53 +1000

    Added Ruby on Rails 3.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Optician::Application.config.secret_token = 'a1e7cd3f58161a07f0b8a640ea83def97ffe3724dc632114ea3d47d1ae4000ea27fcac50d248bc9fbc950d60b340f36c4299372aef6201842b947fd213f94e68'
