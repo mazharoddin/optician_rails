0051538dccdcd15a399485b5d185de37861b91ab
Rich Buggy <rich@buggy.id.au>
2012-10-05 11:52:42 +1000

    Changed Admin to Administration in breadcrumbs

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ApplicationController < ApplicationController
 	protect_from_forgery
 
-	add_breadcrumb "Admin", :admin_root_path
+	add_breadcrumb "Administration", :admin_root_path
 end
