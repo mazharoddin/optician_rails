d9dc79e4ec6b71c66aeedb046a40280356802f66
Rich Buggy <rich@buggy.id.au>
2012-10-26 04:10:31 +1100

    Require employees to be logged in to access the application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,7 @@ require 'BootstrapBreadcrumbsBuilder'
 class ApplicationController < ActionController::Base
 	protect_from_forgery
 
+	before_filter :authenticate_employee!
+
 	add_breadcrumb "Home", :root_path
 end
