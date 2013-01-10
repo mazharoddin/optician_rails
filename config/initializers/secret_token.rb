b6289f6d6c18874ab6a91e17fa4d66252bb83a87
Rich Buggy <rich@buggy.id.au>
2013-01-10 21:47:30 +1100

    Upgraded Rails to 3.2.11

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Optician::Application.config.secret_token = 'a1e7cd3f58161a07f0b8a640ea83def97ffe3724dc632114ea3d47d1ae4000ea27fcac50d248bc9fbc950d60b340f36c4299372aef6201842b947fd213f94e68'
+Optician::Application.config.secret_token = 'c153fbc2f381d08b3fdf7ee2478f345d3e274fa97f65012c773a94b0d415f5382686bb44bd28a7e3f430652a00cec33ab2c052f54b3db0942c497ad704da0d42'
