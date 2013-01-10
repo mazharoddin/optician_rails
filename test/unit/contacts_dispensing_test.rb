0efb001f4b33e9276ea06503966b61ce686811a5
Rich Buggy <rich@buggy.id.au>
2013-01-10 20:59:58 +1100

    Added missing model unit test files

---
+++ test/unit/contacts_dispensing_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ContactsDispensingTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/unit/glasses_dispensing_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GlassesDispensingTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
