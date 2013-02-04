1585f58190915da23bb20a7652287970dce34383
Rich Buggy <rich@buggy.id.au>
2013-02-05 09:15:46 +1100

    Find the current account from the logged in user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,19 @@ require 'BootstrapBreadcrumbsBuilder'
 class ApplicationController < ActionController::Base
 	protect_from_forgery
 
-	before_filter :set_current_account
 	before_filter :authenticate_user!
+	before_filter :set_current_account
 	before_filter :navbar
 
 	add_breadcrumb "Home", :root_path
 	
 	private
 	def set_current_account
-	  @current_account = Account.where('subdomain = ? ', request.subdomains.first).first!
+	  if current_user != nil
+		@current_account = current_user.account
+	  else
+	    @current_account = nil
+	  end
 	end
 	
 	def navbar
